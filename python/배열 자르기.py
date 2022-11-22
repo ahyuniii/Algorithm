65fb1a66151ca74349927a126f558a6e5a8c4e60
ahyuniii <bornthislee@gmail.com>
2022-11-22 21:07:08 +0900

    python

--- python/배열 자르기.py
+++ python/배열 자르기.py
@@ -10,3 +10,19 @@ def solution(numbers, num1, num2):
         answer.append(numbers[i])
         i += 1
     return answer
+
+# 다른 사람 풀이
+def solution(numbers, num1, num2):
+    return numbers[num1:num2 + 1]
+
+def solution(numbers, num1, num2):
+    answer = []
+    for i in range(num1, num2 + 1):
+        answer.append(numbers[i])
+    return answer
+
+def solution(numbers, num1, num2):
+    answer = []
+    for i in numbers[num1 : num2 + 1]:
+        answer.append(i)
+    return answer
