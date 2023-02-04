1e6425cd790321f191cd31c12ce2b4552772f014
ahyuniii <bornthislee@gmail.com>
2023-02-04 22:00:07 +0900

    알고리즘

---
+++ java/baekjoon/문자열/숫자의합_11720.java
@@ -0,0 +1,20 @@
+package 문자열;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 숫자의합_11720 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(input.readLine());
+		String str = input.readLine();
+		int sum = 0;
+		for(int i = 0; i < N; i++) {
+			sum += str.charAt(i) - 48;
+		}
+		System.out.println(sum);
+	}
+
+}
