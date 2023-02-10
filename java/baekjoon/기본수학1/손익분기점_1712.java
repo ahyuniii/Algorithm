5da378184b0744aeeebaff6316c60febaa864527
ahyuniii <bornthislee@gmail.com>
2023-02-10 22:37:49 +0900

    알고리즘

---
+++ java/baekjoon/기본수학1/손익분기점_1712.java
@@ -0,0 +1,26 @@
+package 기본수학1;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+//public class 손익분기점_1712 {
+//
+//	public static void main(String[] args) throws IOException {
+//	BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+//	int A = input.read();
+//	int B = input.read();
+//	int C = input.read();
+//
+//	int BEF;
+//	for(BEF = 0;;BEF++) {
+//		if(C * BEF > (A + B) * C) {
+//			break;
+//		} else {
+//			BEF = -1;
+//		}
+//
+//	}
+//	System.out.println(BEF);
+//	}
+//}
---
+++ java/baekjoon/문자열/그룹단어체커_1316.java
@@ -0,0 +1,33 @@
+package 문자열;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+public class 그룹단어체커_1316 {
+
+//	public static void main(String[] args) throws IOException {
+//		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+//		int n = Integer.parseInt(input.readLine());
+//		String[] str = new String[n];
+//
+//		int count = 0;
+//		for(int i = 0; i < str.length; i++) {
+//			str[i] = input.readLine();
+//			
+//			for(int j = 0; j < str[i].length(); j++) {
+//				if(str[i].contains(String.valueOf(str[i].charAt(j)))) {
+//					count = 0;
+//					str[i] = "X";
+//					break;
+//				}
+//			}
+//		}
+//		for(int i = 0; i < str.length; i++) {
+//			if(str[i] != "X") {
+//				count++;
+//			}
+//			System.out.println(count);
+//		}
+//	}
+//}
+
