fed1a1ae2ffad1ef1a8d218e5783b7ee8d294d95
ahyuniii <bornthislee@gmail.com>
2023-02-12 22:17:47 +0900

    알고리즘

--- java/baekjoon/문자열/그룹단어체커_1316.java
+++ java/baekjoon/문자열/그룹단어체커_1316.java
@@ -7,28 +7,31 @@ public class 그룹단어체커_1316 {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(input.readLine());
-		String[] str = new String[n];
-
-		int count = 0;
-		for(int i = 0; i < str.length; i++) {
-			str[i] = input.readLine();
-			
-			for(int j = 0, int k = 1; j < str[i].length(); j++) {
-				if(str[i].charAt(j) != str[i].charAt(k)) {
-					if(str[i].charAt(j) == str[i].charAt(k)) {
-						str[i] = "X";
-						break;
-					}
-				}
-			}
-		}
-		for(int i = 0; i < str.length; i++) {
-			if(str[i] != "X") {
-				count++;
-			}
-			System.out.println(count);
-		}
-	}
-}
+		
+		int n = Integer.parseInt(input.readLine()); // n = 입력되는 단어 개수
+		
+//		int count = 0; // count = 그룹단어 개수
+//		for(int i = 0; i < n; i++) {
+//			boolean[] check = new boolean[26]; // 중복되는 알파벳이 있는지 확인
+//			String str = input.readLine();
+//			
+//			for(int j = 0; j < str.length(); j++) {
+//				if(str.charAt(j) != str.charAt(j + 1)) {
+//					if(check[str.charAt(j) - 'a'] == false) {
+//						check[str.charAt(j)] = true;
+//					}
+//				}
+//			}
+//			for(int j = 0; j < str.length(); j++) {
+//				if(check[i] = true)
+//			}
+//		}
+//		for(int i = 0; i < str.length(); i++) {
+//			if(str[i] != "X") {
+//				count++;
+//			}
+//			System.out.println(count);
+//		}
+//	}
+//}
 
