a9dced292852ed9985bd631421aae26e6c5015d6
ahyuniii <bornthislee@gmail.com>
2023-02-13 22:09:29 +0900

    알고리즘

---
+++ java/baekjoon/문자열/상수_2908.java
@@ -0,0 +1,20 @@
+package 문자열;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class 상수_2908 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+		
+//		int a = Integer.parseInt(input.readLine());
+//		int b = Integer.parseInt(input.readLine());
+//		
+//		int one = a / 100;
+//		int ten = (a - one * 100) / 10;
+		
+	}
+
+}
